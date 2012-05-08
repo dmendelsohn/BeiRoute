b0717876bae1e848ecc3d1d6bc6aee7b99053dfb
John Bowler <jwbowler@gmail.com>
2012-05-08 01:25:06 -0400

    merging with dan's changes to control

--- src/com/purpleparrots/beiroute/AndroidSerializable.java
+++ src/com/purpleparrots/beiroute/AndroidSerializable.java
@@ -1,13 +1,12 @@
 package com.purpleparrots.beiroute;
 
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.Hashtable;
-import java.util.StringTokenizer;
+
+import android.content.Intent;
 
 import com.google.gson.Gson;
 
@@ -16,6 +15,11 @@ public abstract class AndroidSerializable {
 	private static Gson g = new Gson();
 	private static FileIoService fileIo = new FileIoService();
 	
+	public AndroidSerializable() {
+		//fileIo.startService(new Intent());
+	}
+	
+	/*
 	private static void serialize(int id, AndroidSerializable obj) throws IOException {
 		String json = g.toJson(obj);
 		String FILENAME = "" + id;
@@ -23,8 +27,10 @@ public abstract class AndroidSerializable {
 		g.toJson(obj, osw);
 		osw.close();
 	}
+	*/
 	
 	public static void serializeTable(Hashtable<Integer, AndroidSerializable> table, String tableName) throws IOException {
+		/*
 		String s = "";
 		for (int k: table.keySet()) {
 			serialize(k, table.get(k));
@@ -33,8 +39,14 @@ public abstract class AndroidSerializable {
 		OutputStreamWriter osw = fileIo.getOutputStream(tableName);
 		osw.write(s);
 		osw.close();
+		*/
+		String json = g.toJson(table);
+		OutputStreamWriter osw = fileIo.getOutputStream(tableName);
+		g.toJson(table, osw);
+		osw.close();
 	}
 	
+	/*
 	private static AndroidSerializable deserialize(int id) throws IOException {
 		String FILENAME = "" + id;
 		InputStreamReader isr = fileIo.getInputStream(FILENAME);
@@ -42,8 +54,10 @@ public abstract class AndroidSerializable {
 		isr.close();
 		return obj;
 	}
+	*/
 	
-	public static Hashtable<Integer, AndroidSerializable> deserializeTable(String tableName) throws IOException {
+	public static Hashtable<Integer, AndroidSerializable> deserializeTable(String tableName) {
+		/*
 		Hashtable<Integer, AndroidSerializable> table = new Hashtable<Integer, AndroidSerializable>();
 		InputStreamReader isr = fileIo.getInputStream(tableName);
 		String s = "";
@@ -57,6 +71,21 @@ public abstract class AndroidSerializable {
 		for (int i = 0; i < total; i++) {
 			int k = Integer.parseInt(st.nextToken());
 			table.put(k, deserialize(k));
+		}
+		return table;
+		*/
+		InputStreamReader isr;
+		try {
+			isr = fileIo.getInputStream(tableName);
+		} catch (Exception e) {
+			return new Hashtable<Integer, AndroidSerializable>();
+		}
+		Hashtable<Integer, AndroidSerializable> table = (Hashtable<Integer, AndroidSerializable>) g.fromJson(isr, Hashtable.class);
+		try {
+			isr.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
 		return table;
 	}
--- src/com/purpleparrots/beiroute/FileIoService.java
+++ src/com/purpleparrots/beiroute/FileIoService.java
@@ -1,12 +1,14 @@
 package com.purpleparrots.beiroute;
 
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class FileIoService extends Service {
 
@@ -16,8 +18,17 @@ public class FileIoService extends Service {
 		return null;
 	}
 	
+	public int onStartCommand (Intent intent, int flags, int startId) {
+		return 0;
+	}
+	
 	public OutputStreamWriter getOutputStream(String localPath) throws FileNotFoundException {
-		return new OutputStreamWriter(openFileOutput(localPath, MODE_PRIVATE));
+		Log.w("FileIoService.getOutputStream()", localPath);
+		FileOutputStream fos = openFileOutput(localPath, MODE_PRIVATE);
+		Log.w("FileIoService.getOutputStream()", fos.toString());
+		OutputStreamWriter osw = new OutputStreamWriter(fos);
+		Log.w("FileIoService.getOutputStream()", osw.toString());
+		return osw;
 		
 	}
 	
